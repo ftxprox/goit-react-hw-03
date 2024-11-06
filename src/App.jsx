bae3838605d1d5fef2120594cdd8ddec15367631
HryhorievOA <ftxprox@gmail.com>
2024-11-06 20:58:49 +0200

    add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,36 @@ import './App.css';
 import ContactForm from './components/ContactForm/ContactForm';
 import SearchBox from './components/SearchBox/SearchBox'
 import ContactList from './components/ContactList/ContactList'
-// import Contact from './components/Contact/Contact'
-// import { Formik, Field, Form } from "formik";
-import { useState} from 'react';
+import { useState, useEffect  } from 'react';
 import { nanoid } from 'nanoid'; 
+import { } from 'react';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts ? JSON.parse(savedContacts) : [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+  });
 
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = (newContact) => {
     setContacts((prevContacts) => [...prevContacts, { ...newContact, id: nanoid() }]);
   };
 
   const deleteContact = (contactId) => {
-    setContacts((prevContact) => {
-      return prevContact.filter((contact) => contact.id !== contactId);
-    });
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== contactId));
   };
 
-   const handleFilterChange = (filterValue) => {
+  const handleFilterChange = (filterValue) => {
     setFilter(filterValue);
   };
 
@@ -37,15 +41,14 @@ function App() {
 
   return (
     <>
-<div>
-  <h1>Phone<span>book</span></h1>
-  <ContactForm onAddContact={addContact}/>
-  < SearchBox filter={filter} onFilterChange={handleFilterChange} />
-  <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
-</div>
-
+      <div>
+        <h1>Phone<span>book</span></h1>
+        <ContactForm onAddContact={addContact} />
+        <SearchBox filter={filter} onFilterChange={handleFilterChange} />
+        <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
